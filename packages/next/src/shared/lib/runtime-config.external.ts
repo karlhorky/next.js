98e8408d582d93753eb04003640f7d7ebb0bbab8
devjiwonchoi <devjiwonchoi@gmail.com>
2025-09-25 12:48:46 +0200

    add warnings to runtime-config func
    Co-authored-by: Sebastian "Sebbie" Silbermann <silbermann.sebastian@gmail.com>

--- packages/next/src/shared/lib/runtime-config.external.ts
+++ packages/next/src/shared/lib/runtime-config.external.ts
@@ -4,6 +4,10 @@ let runtimeConfig: any
  * @deprecated Runtime config is deprecated and will be removed in Next.js 16.
  */
 export default () => {
+  // Don't use picocolors here to isolate the external dependency.
+  console.warn(
+    `Detected the usage of runtime config. Runtime config is deprecated and will be removed in Next.js 16.`
+  )
   return runtimeConfig
 }
 
@@ -11,5 +15,9 @@ export default () => {
  * @deprecated Runtime config is deprecated and will be removed in Next.js 16.
  */
 export function setConfig(configValue: any): void {
+  // Don't use picocolors here to isolate the external dependency.
+  console.warn(
+    `Detected the usage of runtime config \`setConfig()\`. Runtime config is deprecated and will be removed in Next.js 16.`
+  )
   runtimeConfig = configValue
 }
